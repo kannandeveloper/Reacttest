00b9a3d8fac3af4f83e2736ef2f81b5d67cbd037
thamaraikannan96 <thamaraikannanmech96.com>
2022-01-26 22:35:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-I am thamaraikannan        </p>
+I am davidbilla        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
